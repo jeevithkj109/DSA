8ed3ec894c86c2ca1ccf1dbfdc1d90da5fffee73
Jeevith <jeevithkj109@gmail.com>
2026-07-23 08:55:47 +0530

    Changes

--- src/com/dsa/dp/FrogJumpWithKDistance/SpaceOptimized.java
+++ src/com/dsa/dp/FrogJumpWithKDistance/SpaceOptimized.java
@@ -5,21 +5,36 @@ package com.dsa.dp.FrogJumpWithKDistance;
  * If the Frog is on the ith staircase,it can jump to any stair (i+1), (i+2),..., (i+k).
  * Your task is to find the minimum total energy used by the frog to reach from the 1st stair to the Nth stair*/
 
-public class BottomUpApproach {
+public class SpaceOptimized {
+
 	private static int minEnergy(int n, int[] heights, int k, int[] dp) {
 		if(n == 0) {
 			return 0;
 		}
 		dp[0] = 0;
 		
-		for(int index = 1; index <= n; index++) {
+		for(int index = 1; index <= k; index++) {
 			int result = Integer.MAX_VALUE;
 			for(int i = 1; i <= k && i <= index; i++) {
 				result = Math.min(result, Math.abs(heights[index]-heights[index-i]) +dp[index-i]);
 			}
 			dp[index] = result;
 		}
-		return dp[n];
+		
+		for(int index = k+1; index <= n; index++) {
+			int result = Integer.MAX_VALUE;
+			
+			for(int i = 1; i <= k; i++) {
+				dp[i-1] = dp[i];	
+			}
+			
+			for(int i = 1; i <= k && i <= index; i++) {
+				result = Math.min(result, Math.abs(heights[index]-heights[index-i]) +dp[k-i]);
+			}
+			dp[k] = result;
+		}
+		
+		return dp[k];
 		
 	}
 
@@ -27,7 +42,10 @@ public class BottomUpApproach {
 		int[] heights = {10, 20, 30,10};
 		int k = 2;
 		int n = heights.length;
-		int[] dp = new int[n];
+		
+		k = Math.min(k, n-1);
+		
+		int[] dp = new int[k+1];
 		System.out.println(minEnergy(n-1, heights, k, dp));
 	}
 }
