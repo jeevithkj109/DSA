84975acf4ee3c78b8134666e69b1d5a6a3047047
Jeevith <jeevithkj109@gmail.com>
2026-08-03 16:45:44 +0530

    Added Max Sum Increasing Subsequence problem

---
+++ src/com/dsa/dp/Strings/SumIncreasingSubsequence/OptimalSolution.java
@@ -0,0 +1,32 @@
+package com.dsa.dp.Strings.SumIncreasingSubsequence;
+
+/* Given an array of positive integers arr[], find the maximum sum of a subsequence such that the elements of the subsequence
+ * form a strictly increasing sequence.
+ * In other words, among all strictly increasing subsequences of the array, return the one with the largest possible sum.*/
+
+public class OptimalSolution {
+
+	public static int maxSumIS(int arr[]) {
+
+		int n = arr.length;
+
+		int[] MSIS = arr.clone();
+		int result = arr[0];
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					MSIS[i] = Math.max(MSIS[i], arr[i] + MSIS[j]);
+				}
+			}
+			result = Math.max(result, MSIS[i]);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 101, 2, 3, 100 };
+
+		System.out.println(maxSumIS(arr));
+	}
+}
